b4a70525c220fc507ac5d16c99b2d0755f68fdf6
Hailey Francis <n01402670@unf.edu>
2023-04-19 20:19:24 -0400

    deleted old models, adding new models

---
+++ server/models/Post.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+var validate = require('mongoose-validator');
+
+//TODO: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+
+var descriptionValidator = [
+    validate({
+      validator: 'isLength',
+      arguments: [1, 500],
+      message: 'Post should be between 1 and 500 characters'
+    }),
+  ];
+
+const postSchema = new Schema(
+    {
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            validate: thoughtValidator,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (date) => new Date(date).toLocaleString()
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const Post = model('post', postSchema);
+
+module.exports = Post;
