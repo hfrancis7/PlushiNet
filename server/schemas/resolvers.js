a50d4aeeb9eef35dd6ae01fd456d3a64b256e884
Hailey Francis <n01402670@unf.edu>
2023-04-23 00:05:22 -0400

    made sure db updated nicely

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
   Query: {
     async getPosts(){
       try{
-        const posts = await Post.find();
+        const posts = await Post.find().sort({createdAt: -1});
         return posts;
       }catch(err){
         throw new Error(err);
