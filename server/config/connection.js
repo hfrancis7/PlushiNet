a213b52117241ed9def9661721f6c7014a25c63a
Hailey Francis <n01402670@unf.edu>
2023-04-24 19:55:15 -0400

    changed server conntection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try{
-        console.log(process.env.MONGO_URI);
-        const mongooseConnection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Sucessfully connected to DB at ${mongooseConnection.connection.host}`)
-    }catch(err){
-        console.log(`Error connecting to DB ${err}`);
-    }
-};
+const connectURL = 
+    //proccess.env.MONGO_URI ||
+    "mongodb://127.0.0.1:27017/plushinetdb";
 
-module.exports = connectDB;
+mongoose.connect(connectURL);
+
+// const connectDB = async () => {
+//     try{
+//         console.log(connectURL);
+//         const mongooseConnection = await mongoose.connect(connectURL);
+//         console.log(`Sucessfully connected to DB at ${mongooseConnection.connection.host}`)
+//     }catch(err){
+//         console.log(`Error connecting to DB ${err}`);
+//     }
+// };
+
+module.exports = mongoose.connection;
