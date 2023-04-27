5772bf3a29d336b8239a4cd34b2e345d8a6d9021
Hailey Francis <n01402670@unf.edu>
2023-04-27 19:31:19 -0400

    trying to fix createPost page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { postImage } from "../api";
+import { useMutation } from '@apollo/client';
 
+import { postImage } from "../api";
+import { CREATE_POST } from '../utils/mutations';
+
+//AntD imports
+import { Button, Checkbox, Form, Input } from 'antd';
 
 function CreatePost() {
     const [imagePreview, setImagePreview] = useState(""); // <- To 
     const [imageFile, setImageFile] = useState({});
     const [imageUrl, setImageUrl] = useState(null);
+    const [body, setBody] = useState("");
+    const [createPost, { error }] = useMutation(CREATE_POST);
+    
+    let navigate = useNavigate();
 
     const handleImagePreview = (e) => {    // <- This will let you preview the uploaded image
         const file = e.target.files[0];
@@ -24,21 +33,23 @@ function CreatePost() {
         }
     };
 
-    let navigate = useNavigate();
-
     const handleSubmit = async () => {    // <- This will send the selected image to our api
         try {
             const res = await postImage({ image: imageFile });
             console.log(res.data.data.imageUrl);
             setImageUrl(res.data.data.imageUrl);
-            navigate('/');
+            console.log(imageUrl);
+            await createPost({
+                variables: {body: body, image: imageUrl}
+            });
+            //navigate('/');
         }
         catch (err) {
             console.log(err)
         }
     }
   return (
-    <div className="App">
+    <form className="">
             <div className="uploadImage">
                 <input
                     type="file"
@@ -46,18 +57,17 @@ function CreatePost() {
                     onChange={(e) => handleImagePreview(e)}
                 />
             </div>
-
-            <button type="submit" onClick={handleSubmit}>
-                Submit
-            </button>
-            <p>{imageUrl}</p>
             <div
                 className="image-preview-div"
                 style={{ display: imagePreview === "" ? "none" : "flex" }}
             >
                 <img src={imagePreview} alt="" />
             </div>
-        </div>
+            <textarea rows="5" cols="33" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+            <button type="submit" onClick={handleSubmit}>
+                Submit
+            </button>
+        </form>
   )
 }
 
