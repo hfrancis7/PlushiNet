7c60ee439b3518bf63645d166917954178377541
Hailey Francis <n01402670@unf.edu>
2023-04-27 15:41:26 -0400

    I GOT AN IMAGE TO UPLOAD TO CLOUDINARY

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -7,19 +7,25 @@ const { cloudinary } = require('../utils/cloudinary');
 const { upload } = require("../utils/multer");
 
 exports.uploadImage = catchAsync(async (req, res, next) => {
-    let cloudinaryResponse;
-    if (req.file) {
-        cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, {
-            upload_preset: 'trials',
-        });
-    } else {
-        return next(new AppError('Please provide a file', 400));
-    }
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            imageUrl: cloudinaryResponse?.url,
-        },
-    });
+    let cloudinaryResponse;
+
+    try{
+        if (req.file) {
+            cloudinaryResponse = await cloudinary.uploader.upload(req.file.path);
+            console.log(cloudinaryResponse);
+        } else {
+            return next(new AppError('Please provide a file', 400));
+        }
+    
+        res.status(200).json({
+            status: 'success',
+            data: {
+                imageUrl: cloudinaryResponse?.url,
+            },
+        });
+    }catch(err){
+        throw new Error(err);
+    }
 });
+
