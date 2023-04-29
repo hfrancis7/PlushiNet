52ed63e48c1057d29868bf5cc64a28383334d4ef
Hailey Francis <n01402670@unf.edu>
2023-04-29 16:59:36 -0400

    recommitting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectURL =
-    process.env.MONGO_URI ||
+    process.env.MONGO_URI || //make sure to change your env
     "mongodb://127.0.0.1:27017/plushinetdb";
 
 mongoose.connect(connectURL);
