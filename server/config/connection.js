d889a51bf93a5b692dcaf339d8d2208943e82b97
Hailey Francis <n01402670@unf.edu>
2023-04-29 16:57:18 -0400

    fixed product relationships, added seeds, seeds fill sucessfully

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectURL =
-    // process.env.MONGO_URI ||
+    process.env.MONGO_URI ||
     "mongodb://127.0.0.1:27017/plushinetdb";
 
 mongoose.connect(connectURL);
