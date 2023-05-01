6f71462485c9c06da56ccd882a54363553e0f271
Hailey Francis <n01402670@unf.edu>
2023-04-30 21:56:33 -0400

    changing css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ function Home() {
   return (
     <div>
       <MainImage />
+      <h2>plushi [plush-ee] <br></br>noun: a toy that is covered in plush fabric and filled with soft material. <br></br>Also called: plush, plushy, plushie </h2>
+      <h3 className="directions">Been here before? Sign In! <br></br>Are you new here? Sign Up to get Started! <br></br>Click the “All Plushies” tab and Happy Posting!</h3>
       <Carousel/>
     </div>
   )
